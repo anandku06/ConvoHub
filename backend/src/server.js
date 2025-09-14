ef10010478447f527a1dbf9f1a3a3bc400cce22f
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-09-15 00:27:36 +0530

    feat: add database connection logic and update server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { ENV } from "./config/env";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 
 const app = express();
@@ -10,4 +11,5 @@ app.get("/", (req, res) => {
 
 app.listen(ENV.PORT, () => {
   console.log(`Server is listening on port ${ENV.PORT}`);
+  connectDB();
 });
