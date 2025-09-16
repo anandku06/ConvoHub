604f492fd5edf2f624cb95c62f71abe404f349b0
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-09-17 00:16:23 +0530

    feat: update package.json and package-lock.json for @clerk dependencies

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,7 +1,9 @@
-import "dotenv/config"
+import "dotenv/config";
 
 export const ENV = {
-    PORT : process.env.PORT || 3000,
-    MONGODB_URI : process.env.MONGODB_URI,
-    NODE_ENV : process.env.NODE_ENV, 
-}
+  PORT: process.env.PORT || 3000,
+  MONGODB_URI: process.env.MONGODB_URI,
+  NODE_ENV: process.env.NODE_ENV,
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+};
