6d569139b92f400a0aeaacda2b229339a0f09929
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-09-18 23:25:14 +0530

    feat: integrate @clerk/clerk-react for authentication and add publishable key handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <header>
+      <SignedOut>
+        <SignInButton mode="modal" />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+};
 
-export default App
+export default App;
