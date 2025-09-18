013b9965948c64c57abb67db89cbed80f17a0b5b
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-09-19 00:43:25 +0530

    feat: implement Inngest functions for user creation and deletion, and set up user model

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,13 +1,37 @@
 import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "convohub" });
 
 const syncUser = inngest.createFunction(
-    {id : "sync-user"},
-    {event : "clerk/user.created"},
-    async (event) => {}
-)
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
+
+    const { id, email_addresses, first_name, last_name, image_url } =
+      event.data;
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}`,
+      image: image_url,
+    };
+  }
+);
+
+const deleteUser = inngest.createFunction(
+  { id: "delete-user" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
+
+    const { id } = event.data;
+    // Delete user from database
+    await User.deleteOne({ clerkId: id });
+  }
+);
 
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [syncUser, deleteUser];
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,32 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
-import {clerkMiddleware} from "@clerk/express";
-
+import { clerkMiddleware } from "@clerk/express";
+import { inngest, functions } from "./config/inngest.js";
+import { serve } from "inngest/express";
 
 const app = express();
 
 app.use(express.json());
-app.use(clerkMiddleware()) // Clerk middleware to handle authentication ; used to protect routes
+app.use(clerkMiddleware()); // Clerk middleware to handle authentication ; used to protect routes
+
+// Inngest setup
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server is listening on port ${ENV.PORT}`);
-  connectDB();
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log(`Server is listening on port ${ENV.PORT}`);
+      });
+    }
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
