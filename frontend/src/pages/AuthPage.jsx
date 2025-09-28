2f8c7b104913a6942382266c34e96fbfee4efbe8
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-09-28 23:59:51 +0530

    feat: implement routing and create AuthPage and HomePage components

---
+++ frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,12 @@
+import { SignInButton } from '@clerk/clerk-react'
+import React from 'react'
+
+const AuthPage = () => {
+  return (
+    <div>
+        <SignInButton />
+    </div>
+  )
+}
+
+export default AuthPage
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import { UserButton } from '@clerk/clerk-react'
+import React from 'react'
+
+export const HomePage = () => {
+  return (
+    <div>
+        <UserButton />
+        Home Page
+    </div>
+  )
+}
